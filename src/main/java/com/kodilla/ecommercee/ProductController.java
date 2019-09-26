dbac419dacaa0d68bb4a446eb1b0c110f53dc152
gawelx <gawelx@gmail.com>
2019-09-26 23:48:57 +0200

    ProductController - wstępna implementacja endpointów (poprawiona)
    Kontroler został uproszczony - teraz zwraca po prostu przykładowe dane.
    Zgodnie z sugestią Pawła Pluty kontroler zawiera metodę, która
    generuje przykładowe dane na potrzeby testów.
    W klasie ProductDto zamieniłem konstruktory i gettery na stosowne
    andotacje Lomboka.

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,8 +1,6 @@
 package com.kodilla.ecommercee;
 
 import com.kodilla.ecommercee.dto.ProductDto;
-import com.kodilla.ecommercee.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,42 +10,54 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/products")
 public class ProductController {
 
-    private final ProductService productService;
-
-    @Autowired
-    public ProductController(final ProductService productService) {
-        this.productService = productService;
-    }
-
     @GetMapping
     public List<ProductDto> getAllProducts() {
-        return productService.getProducts();
+        return getSampleData();
+    }
+
+    private List<ProductDto> getSampleData() {
+        String[] names = {"kurtka zimowa", "płaszcz", "buty", "rękawiczki", "sandały", "zegarek", "złote kolczyki"};
+        String description = "Pellentesque tempus interdum quam ut rhoncus. Donec ullamcorper turpis dolor.";
+        int[] prices = {100, 150, 100, 50, 80, 250, 300};
+        long[] groupIds = {1, 1, 4, 2, 4, 3, 3};
+        List<ProductDto> sampleProducts = new ArrayList<>();
+        for (int id = 0; id < names.length; id++) {
+            sampleProducts.add(new ProductDto(
+                    (long) (id + 1),
+                    names[id],
+                    description,
+                    new BigDecimal(prices[id]),
+                    groupIds[id]
+            ));
+        }
+        return sampleProducts;
     }
 
     @GetMapping("/{id}")
     public ProductDto getProduct(@PathVariable Long id) {
-        return productService.getProduct(id);
+        return getSampleData().get(0);
     }
 
     @PostMapping
-    public ProductDto createProduct(@RequestBody ProductDto productDto) {
-        return productService.createProduct(productDto);
+    public ProductDto createProduct(@RequestBody ProductDto product) {
+        return product;
     }
 
     @PutMapping
-    public ProductDto updateProduct(@RequestBody ProductDto productDto) {
-        return productService.updateProduct(productDto);
+    public ProductDto updateProduct(@RequestBody ProductDto product) {
+        return product;
     }
 
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable Long id) {
-        productService.deleteProduct(id);
     }
 
 }
--- src/main/java/com/kodilla/ecommercee/dto/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/ProductDto.java
@@ -1,7 +1,14 @@
 package com.kodilla.ecommercee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class ProductDto {
 
     private Long id;
@@ -9,40 +16,5 @@ public class ProductDto {
     private String description;
     private BigDecimal price;
     private Long groupId;
-
-    public ProductDto() {
-    }
-
-    public ProductDto(Long id, String name, String description, BigDecimal price, Long groupId) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.groupId = groupId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public Long getGroupId() {
-        return groupId;
-    }
 
 }
