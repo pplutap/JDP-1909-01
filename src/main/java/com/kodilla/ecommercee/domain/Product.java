aea83e7e983bb22b10bb4986f815c0de68a17ef8
gawelx <gawelx@gmail.com>
2019-10-04 08:19:47 +0200

    Encja product - implementacja (poprawka do pola name)
    Uzuepłnione ograniczenie unikalności na polu name.

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -23,6 +24,7 @@ public class Product {
     private Long id;
 
     @NotNull
+    @Column(unique = true)
     private String name;
 
     private String description;
