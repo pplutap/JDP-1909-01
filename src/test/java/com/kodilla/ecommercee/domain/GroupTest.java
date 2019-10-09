c9bfac1507d2ab007f7b4d01313a1c2e0dd07ee6
Arelias <robertmusialik95@gmail.com>
2019-10-09 17:33:57 +0200

    All CRUD tests added

--- src/test/java/com/kodilla/ecommercee/domain/GroupTest.java
+++ src/test/java/com/kodilla/ecommercee/domain/GroupTest.java
@@ -1,19 +1,16 @@
 package com.kodilla.ecommercee.domain;
 
-import com.kodilla.ecommercee.GenericEntity;
-import com.kodilla.ecommercee.GenericEntityRepository;
 import com.kodilla.ecommercee.repository.GroupRepository;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
-
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GroupTest {
@@ -21,8 +18,24 @@ public class GroupTest {
     @Autowired
     private GroupRepository groupRepository;
 
+    @After
+    public void clearRepository(){
+        groupRepository.findAll().clear();
+    }
+
     @Test
-    public void testSaveAndGet() {
+    public void testSave() {
+        //Given
+        Group group = new Group(1L, "Chemia");
+        groupRepository.save(group);
+        //When
+        int groupSize = groupRepository.findAll().size();
+        //Then
+        assertEquals(1, groupSize);
+    }
+
+    @Test
+    public void testGet() {
         //Given
         Group group = new Group(1L, "Chemia");
         groupRepository.save(group);
@@ -30,5 +43,30 @@ public class GroupTest {
         Optional<Group> group2 = groupRepository.findById(group.getId());
         //Then
         assertEquals("Chemia", group2.get().getName());
+    }
+
+    @Test
+    public void testUpdate() {
+        //Given
+        Group group = new Group(1L, "Chemia");
+        groupRepository.save(group);
+        group = new Group(groupRepository.findAll().get(0).getId(), "Nowa Chemia");
+        groupRepository.save(group);
+        //When
+        Optional<Group> updatedGroup = groupRepository.findById(group.getId());
+        //Then
+        assertEquals("Nowa Chemia", updatedGroup.get().getName());
+        assertEquals(1, groupRepository.findAll().size());
+    }
+
+    @Test
+    public void testDelete() {
+        //Given
+        Group group = new Group(1L, "Chemia");
+        groupRepository.save(group);
+        //When
+        groupRepository.deleteById(group.getId());
+        //Then
+        assertEquals(0, groupRepository.findAll().size());
     }
 }
