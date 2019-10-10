8dec33a08883b0f095d9be6427e9cdfd9f5d7fba
gawelx <gawelx@gmail.com>
2019-10-10 10:17:35 +0200

    Testy encji `Order` - uzupełnienie adnotacji w repozytorium
    W repozytorium `OrderRepository` zostały uzupełnione brakujące adnotacje
    `@Repository` oraz `@Transactional`.

--- src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
@@ -2,6 +2,11 @@ package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
