3ec669b3f4b457d17d8489cb61bb53317734c6b1
szalajm <49290577+szalajm@users.noreply.github.com>
2019-10-10 19:36:50 +0200

    Update Order.java

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
