0dc2ac44817fce3932cff7a199cd9e2132686140
gawelx <gawelx@gmail.com>
2019-10-10 10:18:34 +0200

    Testy encji `Order` - pierwsze testy (WIP)
    Dwa pierwsze testy. Drugi test jeszcze nie działa.

---
+++ src/test/java/com/kodilla/ecommercee/repository/OrderRepositoryTestSuite.java
@@ -0,0 +1,99 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.Order;
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Sql({
+        "/sql/groups.sql",
+        "/sql/products.sql",
+        "/sql/users.sql",
+        "/sql/orders.sql"
+})
+@Transactional
+public class OrderRepositoryTestSuite {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testCreateNewOrderIfGivenValidOrderObject() {
+        //Given
+        Product product1 = productRepository.getOne(11L);
+        Product product2 = productRepository.getOne(12L);
+        User buyer = userRepository.getOne(21L);
+        User seller = userRepository.getOne(22L);
+        Order newOrder = new Order(
+                null,
+                buyer,
+                seller,
+                LocalDateTime.of(2019, 9, 1, 12, 15),
+                Arrays.asList(product1, product2),
+                "AWAITING PAYMENT",
+                LocalDateTime.of(2019, 9, 1, 12, 15)
+        );
+
+        //When
+        long ordersCountBeforeCreation = orderRepository.count();
+        Order createdOrder = orderRepository.save(newOrder);
+        long ordersCountAfterCreation = orderRepository.count();
+        List<Order> retrievedOrders = orderRepository.findAll();
+
+        //Then
+        assertThat(ordersCountAfterCreation, is(equalTo(ordersCountBeforeCreation + 1)));
+        assertThat(retrievedOrders, hasItem(createdOrder));
+    }
+
+    @Test
+    public void testThrowExceptionIfGivenInvalidOrderObject() {
+        //Given
+        Order invalidOrder = new Order(
+                null,
+                null,
+                null,
+                null,
+                new ArrayList<>(),
+                null,
+                null
+        );
+
+        //When
+        Exception exception = null;
+        Order createdOrder = null;
+        try {
+            createdOrder = orderRepository.save(invalidOrder);
+        } catch (Exception e) {
+            exception = e;
+        }
+
+        //Then
+        assertThat(exception, is(notNullValue()));
+    }
+
+}
