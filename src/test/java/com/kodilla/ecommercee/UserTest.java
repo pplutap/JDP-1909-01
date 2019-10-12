3710e6d626417686e12262755eb258a9ceb0df12
Marcin Bilant <marcinb@fabelo.local>
2019-10-12 14:50:58 +0200

    JDP190901-20 testy User v.01

--- src/test/java/com/kodilla/ecommercee/UserTest.java
+++ src/test/java/com/kodilla/ecommercee/UserTest.java
@@ -10,44 +10,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Optional;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserTest {
+
 
     @Autowired
     private UserRepository userRepository;
 
 
     @Test
-    public void testUserReadId() {
+    public void testUserRead() {
         //Given
-        User user = new User(1l, "Jan", 1, 123456L);
+        User user1 = new User(20l, "Waldek", 1, 144456L);
 
         //When
-        userRepository.save(user);
+        User user2 = userRepository.save(user1);
 
         //Then
-        long id = user.getId();
+        long id = user2.getId();
         Optional<User> readUser = userRepository.findById(id);
-        String userName = readUser.get().getUserName();
-        Assert.assertEquals("Jan", userName);
+        String  userName = readUser.get().getUserName();
+        Assert.assertEquals("Waldek", userName);
 
+        //CleanUp
+        userRepository.deleteById(id);
     }
 
     @Test
     public void testUserAdd() {
         //Given
-        User user = new User(1l, "Jan", 1, 123456L);
+        User user1 = new User(20l, "Jan", 1, 123456L);
 
         //When
-        userRepository.save(user);
+        User user2 = userRepository.save(user1);
 
         //Then
-        long id = user.getId();
+        long id = user2.getId();
         Optional<User> readUser = userRepository.findById(id);
         Assert.assertTrue(readUser.isPresent());
+
+        //CleanUp
+        userRepository.deleteById(id);
 
     }
 
@@ -55,11 +64,11 @@ public class UserTest {
     @Test
     public void testUserUpdate() {
         //Given
-        User user = new User(1L, "Jan", 1, 123456L);
-        userRepository.save(user);
-        user = new User(userRepository.findAll().get(0).getId(),"Henryk", 1, 123456L);
-        long id = user.getId();
-        userRepository.save(user);
+        User user1 = new User(1l,"Jerzy", 1, 987654L);
+        User user2 = userRepository.save(user1);
+        long id = user2.getId();
+        user1 = new User(id,"Henryk", 1, 987654L);
+        userRepository.save(user1);
 
         //When
         Optional<User> readUser = userRepository.findById(id);
@@ -67,18 +76,21 @@ public class UserTest {
         //Then
        Assert.assertEquals("Henryk", readUser.get().getUserName());
 
+        //CleanUp
+        userRepository.deleteById(id);
+
 
     }
     @Test
     public void testUserDelete() {
         //Given
-        User user = new User(1l, "Jan", 1, 123456L);
+        User user1 = new User(1l, "Kuba", 1, 654321L);
 
         //When
-        userRepository.save(user);
+        User user2 = userRepository.save(user1);
 
         //Then
-        long id = user.getId();
+        long id = user2.getId();
         Optional<User> readUser = userRepository.findById(id);
         Assert.assertTrue(readUser.isPresent());
         userRepository.deleteById(id);
