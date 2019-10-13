2db8efcd281d35ae1101cd57c9c8b029dab12115
gawelx <gawelx@gmail.com>
2019-10-13 20:34:00 +0200

    User - dodanie do encji pól session
    Przy okazji uzupełnione zostało pole `password` w klasie `UserDto` oraz
    utworzona nowa klasa `CredentialsDto` służąca do przesyłania do
    kontrolera danych do logowania.

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,8 +1,15 @@
 package com.kodilla.ecommercee.domain;
-import lombok.*;
 
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -19,10 +26,17 @@ public class User {
     private String userName;
 
     @NotNull
+    private String password;
+
+    @NotNull
     private Integer status;
 
     @NotNull
     @Column(unique = true)
     private Long userKey;
+
+    private String sessionKey;
+
+    private LocalDateTime sessionKeyExpireAt;
 
 }
