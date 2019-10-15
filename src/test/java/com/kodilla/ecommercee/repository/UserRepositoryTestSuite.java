cc4c45175aaa21b905331b39090a6b31d8a9a855
gawelx <gawelx@gmail.com>
2019-10-15 22:35:22 +0200

    Testy encji User - zmiany w testach dostosowujące je do zmian w samej encji

--- src/test/java/com/kodilla/ecommercee/repository/UserRepositoryTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/repository/UserRepositoryTestSuite.java
@@ -1,8 +1,7 @@
-package com.kodilla.ecommercee;
+package com.kodilla.ecommercee.repository;
 
 
 import com.kodilla.ecommercee.domain.User;
-import com.kodilla.ecommercee.repository.UserRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,24 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.Optional;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UserTest {
-
+public class UserRepositoryTestSuite {
 
     @Autowired
     private UserRepository userRepository;
 
-
     @Test
     public void testUserRead() {
         //Given
-        User user1 = new User(20l, "Waldek", 1, 144456L);
+        User user1 = new User(20L, "Waldek", "wald434", 1, 144456L, null, null);
 
         //When
         User user2 = userRepository.save(user1);
@@ -45,7 +40,7 @@ public class UserTest {
     @Test
     public void testUserAdd() {
         //Given
-        User user1 = new User(20l, "Jan", 1, 123456L);
+        User user1 = new User(20l, "Jan", "jan999", 1, 123456L, null, null);
 
         //When
         User user2 = userRepository.save(user1);
@@ -57,17 +52,15 @@ public class UserTest {
 
         //CleanUp
         userRepository.deleteById(id);
-
     }
-
 
     @Test
     public void testUserUpdate() {
         //Given
-        User user1 = new User(1l,"Jerzy", 1, 987654L);
+        User user1 = new User(1l, "Jerzy", "jurek32", 1, 987654L, null, null);
         User user2 = userRepository.save(user1);
         long id = user2.getId();
-        user1 = new User(id,"Henryk", 1, 987654L);
+        user1 = new User(id, "Henryk", "henry321", 1, 987654L, null, null);
         userRepository.save(user1);
 
         //When
@@ -78,13 +71,12 @@ public class UserTest {
 
         //CleanUp
         userRepository.deleteById(id);
-
-
     }
+
     @Test
     public void testUserDelete() {
         //Given
-        User user1 = new User(1l, "Kuba", 1, 654321L);
+        User user1 = new User(1l, "Kuba", "kubi567", 1, 654321L, null, null);
 
         //When
         User user2 = userRepository.save(user1);
@@ -96,7 +88,6 @@ public class UserTest {
         userRepository.deleteById(id);
         Optional<User> deleteUser = userRepository.findById(id);
         Assert.assertFalse(deleteUser.isPresent());
-
     }
 
 }
